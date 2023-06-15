75dd8ed35fa83f00607ca922d46fbd7f96c2bc8d
joylan dsouza <joylan.dsouza@datenwissen.com>
2023-06-15 08:29:02 +0530

    code updated

--- Book_N_Joy/main/admin.py
+++ Book_N_Joy/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Books)
+admin.site.register(AboutUs)
+admin.site.register(ContactUs)
+admin.site.register(BookOrder)
+admin.site.register(UserMessage)
--- Book_N_Joy/main/views.py
+++ Book_N_Joy/main/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+from django.urls import reverse
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        try:
+            data = request.POST
+            print(data)
+            book = Books.objects.get(book_name=data['book'])
+            BookOrder.objects.create(email = data['email'],book=book,message=data['message'],contact_number=data['phone'],address=data['address'])
+            return redirect(reverse('home'))
+        except :
+            return redirect(reverse('home'))
     return render(request, 'index.html')
 
 def AboutUs(request):
     return render(request, 'aboutUs.html')
 
-def ContactUs(request):
-    return render(request, 'contactUs.html')
+def ContactUs(request,message=None):
+    context={'message':''}
+    if request.method == 'POST':
+        try:
+            data = request.POST
+            print(data)
+            UserMessage.objects.create(email = data['email'],name=data['name'],message=data['message'])
+            context={'message':'Form submitted successfully'}
+        except Exception as e:
+            print(e)
+
+    return render(request, 'contactUs.html',context=context)
