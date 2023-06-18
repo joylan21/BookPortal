71a7d4121e7422b7e236180e0d120f3ff4e8734e
joylan dsouza <joylan.dsouza@datenwissen.com>
2023-06-18 21:15:41 +0530

    code updated

--- Book_N_Joy/main/models.py
+++ Book_N_Joy/main/models.py
@@ -7,16 +7,25 @@ class Books(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='books')
 
+    def __str__(self):
+        return self.book_name
+
 class AboutUs(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     image_top = models.ImageField(upload_to='about_us')
     image_bottom = models.ImageField(upload_to='about_us')
 
+    def __str__(self):
+        return self.title
+
 class ContactUs(models.Model):
     address = models.TextField()
     phone = models.CharField(max_length=20)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.email
 
 class BookOrder(models.Model):
     book = models.ForeignKey(Books,on_delete=models.CASCADE)
@@ -25,7 +34,13 @@ class BookOrder(models.Model):
     address=models.TextField()
     message = models.TextField()
 
+    def __str__(self):
+        return self.book.book_name
+
 class UserMessage(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
+
+    def __str__(self):
+        return self.name
