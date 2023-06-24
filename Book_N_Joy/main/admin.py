51dcfa845820acb1151ffc6c444617f714427725
joylan dsouza <joylan.dsouza@datenwissen.com>
2023-06-24 19:47:35 +0530

    Revert "code updated"
    This reverts commit a72f73afff860d522a1baa9cb04cb5acb903aa50.

--- Book_N_Joy/main/admin.py
+++ Book_N_Joy/main/admin.py
@@ -3,11 +3,8 @@ from .models import *
 # Register your models here.
 admin.site.register(AboutUs)
 admin.site.register(ContactUs)
+admin.site.register(BookOrder)
 admin.site.register(UserMessage)
-
-class BookOrderAdmin(admin.ModelAdmin):
-    list_display = ('book', 'email', 'contact_number', 'address')
-admin.site.register(BookOrder, BookOrderAdmin)
 
 class BooksAdmin(admin.ModelAdmin):
     readonly_fields = ('slug', 'meta_title', 'meta_description')
